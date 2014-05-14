f2e8c21df7cb857d00ea0e8c0fa104ca64fcacc0
James Hay <jwhay@live.com>
2014-05-14 16:58:00 +0200

    RootURL fixed

--- WebCrawler/WebCrawler/Model/Downloader.cs
+++ WebCrawler/WebCrawler/Model/Downloader.cs
@@ -15,17 +15,26 @@ namespace WebCrawler.Model
         {
             WebClient wc = new WebClient();
             String html = wc.DownloadString(fetchUrl);
+            System.IO.File.WriteAllText("..\\..\\..\\..\\TestFiles\\WebCrawler.html", html);
+
             List<String> links = new List<string>();
             Regex regx = new Regex(@"((http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?)", RegexOptions.IgnoreCase);
             //String regx = "href=\"(.+)\"";
 
+            int count = 0;
             foreach (Match match in regx.Matches(html))
             {
                 links.Add(match.Value);
+                count++;
+                string sub = fetchUrl.Substring(7, 3);
+                Console.WriteLine("Downloading " + match.Value);
+
+                //System.IO.File.WriteAllText("..\\..\\..\\..\\TestFiles\\Download\\" + sub + "\\" + count + ".html", wc.DownloadString(match.Value));
+                System.IO.File.WriteAllText("..\\..\\..\\..\\TestFiles\\Download\\" + count + ".html", wc.DownloadString(match.Value));
             }
 
-            System.IO.File.WriteAllText("..\\..\\..\\..\\TestFiles\\WebCrawler.html", html);
         }
+
 
     }
 }
